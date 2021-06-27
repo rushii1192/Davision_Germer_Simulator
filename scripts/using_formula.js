fdc29adc513a611a1846e43463d8a96710f030c0
Rushi <68375572+rushii1192@users.noreply.github.com>
2021-06-27 21:16:38 +0530

    Create using_formula.js

---
+++ scripts/using_formula.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+
+function reading(voltage,angle,crystal){
+    if(crystal="Nickel"){
+        var d = 0.19
+        console.log("Spacce lattice"+d)
+    }
+}
+
+reading(24,30,"Nickel")
